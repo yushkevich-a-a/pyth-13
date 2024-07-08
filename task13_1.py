1323851bf6a7b73d54cf6cbf1c057998be35414b
Anton <yushkevich.a.a@yandex.ru>
2024-07-08 17:04:21 +0300

    выполнение первой задачи

---
+++ task13_1.py
@@ -0,0 +1,35 @@
+# matrix1 = [
+#   [1,2,3,4],
+#   [5,6,7,8],
+#   [9,10,11,12],
+#   [13,14,15,16],
+#   ]
+# matrix2 = [
+#   [-13,14,15,16],
+#   [-9,10,11,12],
+#   [-5,6,7,8],
+#   [-1,2,3,4],
+# ]
+
+# matrix3 = []
+
+
+matrix1 = [ [3, 5, 1], [8, 7, 2] ]
+matrix2 = [ [5, 3, 4], [1, 10, 9] ]
+matrix3 = []
+
+
+print(type(matrix1))
+# for key, value in matrix1.iteritems() :
+#     print (key, value)
+  # newlist = []
+  # for j in matrix1[i].keys():
+  #   a = matrix1[i][j]
+  #   print(a)
+
+
+
+
+
+
+# print(matrix1 +matrix2)
